3f4e63ea06bdf883334b3c25ae5785da9decc1d6
paulnarup <pnarup@luc.edu>
2016-04-27 22:41:49 -0500

    Update Final_project.cpp

--- Final_project.cpp
+++ Final_project.cpp
@@ -1,9 +1,6 @@
 //============================================================================
 // Name        : Final_project.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Paul Narup
 //============================================================================
 
 #include <iostream>
